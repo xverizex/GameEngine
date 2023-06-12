36beb0bdd7da448ee64f9fae60275f1b7979b1bd
Dmitrii <horisontdawn@yandex.ru>
2023-06-12 17:21:15 +0500

    [*] fix

--- src/Game/Core/GameConfig.cpp
+++ src/Game/Core/GameConfig.cpp
@@ -12,6 +12,7 @@
 #include <Game/Levels/Tutorial.h>
 
 #include <cstdio>
+#include "Engine/Core/Downloader.h"
 
 GameConfig* GameConfig::get_instance ()
 {
@@ -20,17 +21,55 @@ GameConfig* GameConfig::get_instance ()
 	return instance;
 }
 
+void GameConfig::unload_res ()
+{
+    ShaderManager* shader_manager = ShaderManager::get_instance ();
+    shader_manager->free_programs ();
+
+    downloader_clear_memory_of_sprites ();
+
+    is_loaded = false;
+}
+
+void GameConfig::load_res (uint32_t i, bool with_shaders)
+{
+    if (is_loaded)
+        return;
+
+
+    if (with_shaders) {
+        ShaderManager* shader_manager = ShaderManager::get_instance ();
+
+        shader_manager->set_shader_size (N_SHADERS);
+
+
+        std::vector<ShaderInfoFile> list_shaders;
+        list_shaders.push_back ({SHADER_RECT, new ShaderRect(), RES_RECT_VERT, RES_RECT_FRAG});
+        list_shaders.push_back ({SHADER_SPRITE, new Shader2DSprite(), RES_SPRITE_VERT, RES_SPRITE_FRAG});
+        list_shaders.push_back ({SHADER_SPRITE_WITH_CAMERA, new Shader2DSpriteWithCamera(), RES_SPRITE_WITH_CAMERA_VERT, RES_SPRITE_WITH_CAMERA_FRAG});
+        list_shaders.push_back ({SHADER_TEXT, nullptr, RES_TEXT_VERT, RES_TEXT_FRAG});
+
+        shader_manager->set_shaders_and_compile (list_shaders);
+    }
+
+    if (i == 0xffff) {
+        is_loaded = true;
+        return;
+    }
+
+
+    downloader_init_sprite_array (N_TEXTURES_RES);
+
+    downloader_download_all_sprites_without_progressbar_by_range (0, N_TEXTURES_RES);
+
+    is_loaded = true;
+}
+
+
 void GameConfig::init_shaders ()
 {
-	std::vector<ShaderInfoFile> list_shaders;
-	list_shaders.push_back ({SHADER_RECT, new ShaderRect(), RES_RECT_VERT, RES_RECT_FRAG});
-	list_shaders.push_back ({SHADER_SPRITE, new Shader2DSprite(), RES_SPRITE_VERT, RES_SPRITE_FRAG});
-	list_shaders.push_back ({SHADER_SPRITE_WITH_CAMERA, new Shader2DSpriteWithCamera(), RES_SPRITE_WITH_CAMERA_VERT, RES_SPRITE_WITH_CAMERA_FRAG});
-	list_shaders.push_back ({SHADER_TEXT, nullptr, RES_TEXT_VERT, RES_TEXT_FRAG});
 
-	ShaderManager* shader_manager = ShaderManager::get_instance ();
-
-	shader_manager->set_shaders_and_compile (list_shaders);
+    load_res(0xffff, true);
 }
 
 void GameConfig::init_screen ()
@@ -44,26 +83,20 @@ void GameConfig::init_screen ()
 void GameConfig::list_of_levels ()
 {
 	n_levels.resize (N_LEVELS);
-#if 0
 	n_levels[LEVEL_GAME] = new GameLevel ();
 	n_levels[LEVEL_SELECT_GAME] = new SelectLevel ();
 	n_levels[LEVEL_MENU] = new MenuLevel ();
 	n_levels[LEVEL_TUTORIAL] = new TutorialLevel ();
-#endif
 }
 
 void GameConfig::entry_point ()
 {
-#if 0
 	cur_level = n_levels[LEVEL_MENU];
-
-	cur_level->load_assets ();
-#endif
 }
 
 void GameConfig::switch_level ()
 {
-	cur_level->unload_assets ();
+	cur_level->unload_level ();
 	cur_level = n_levels[cur_level->new_level];
-	cur_level->load_assets ();
+	cur_level->load_level ();
 }
